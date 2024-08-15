a02d5683fe0b689e53fc86755a1d245a04f0d192
trannhon70 <kevintran351996@gmail.com>
2024-08-15 16:03:33 +0700

    update

--- index.php
+++ index.php
@@ -1,55 +1,128 @@
 <?php include 'inc/header.php' ?>
 <link rel="stylesheet" href="<?php echo $local ?>/css/trang-chu.min.css">
 </head>
+
 <body>
     <?php include 'layout/header_component.php' ?>
     <main>
         <?php include 'layout/slider_component.php' ?>
 
-        <section id="we__bring" class="we__bring" >
-            <div class="we__bring-body" >
-                <div class="we__bring-list" >
+        <section id="we__bring" class="we__bring">
+            <div class="we__bring-body">
+                <div class="we__bring-list">
                     <div class="we__bring-item">
                         <amp-img class="we__bring-item-logo" src="<?php echo $local ?>/images/icons/icon_nam_khoa2.webp" height="260px" width="125px" alt="..."></amp-img>
-                        <div class="we__bring-item-body" >
+                        <div class="we__bring-item-body">
                             <h5>NAM KHOA</h5>
                             <span>Chuyên khoa Nam khoa, Phòng khám Đa khoa Nhật Việt luôn được chú trọng phát triển, nhằm mang lại chất lượng và sự hài lòng nhất cho quý nam giới.</span>
                         </div>
                     </div>
                     <div class="we__bring-item">
                         <amp-img class="we__bring-item-logo" src="<?php echo $local ?>/images/icons/icon_da_lieu2.webp" height="260px" width="125px" alt="..."></amp-img>
-                        <div class="we__bring-item-body" >
+                        <div class="we__bring-item-body">
                             <h5>DA LIỄU</h5>
                             <span>Chuyên khoa Da liễu Phòng khám Đa khoa Nhật Việt là đơn vị đảm nhận chẩn đoán và điều trị các bệnh lý thuộc chuyên ngành Da liễu.</span>
                         </div>
                     </div>
                     <div class="we__bring-item">
                         <amp-img class="we__bring-item-logo" src="<?php echo $local ?>/images/icons/icon_benh_xh2.webp" height="260px" width="125px" alt="..."></amp-img>
-                        <div class="we__bring-item-body" >
+                        <div class="we__bring-item-body">
                             <h5>BỆNH XÃ HỘI</h5>
                             <span>Đa khoa Nhật Việt là một trong những địa chỉ thăm khám, xét nghiệm và điều trị bệnh xã hội uy tín tại TPHCM được người dân thành phố và khu vực tin chọn.</span>
                         </div>
                     </div>
                     <div class="we__bring-item">
                         <amp-img class="we__bring-item-logo" src="<?php echo $local ?>/images/icons/icon_hm_tt2.webp" height="260px" width="125px" alt="..."></amp-img>
-                        <div class="we__bring-item-body" >
+                        <div class="we__bring-item-body">
                             <h5>HẬU MÔN - TRỰC TRÀNG</h5>
                             <span>Khoa Hậu môn – Trực tràng Phòng khám Đa khoa Nhật Việt chuyên cung cấp dịch vụ khám chữa toàn diện các bệnh lý hậu môn – trực tràng uy tín.</span>
                         </div>
                     </div>
                 </div>
                 <hr>
-                <div class="we__bring-row" >
-                    <div class="we__bring-row-left" >
-                    <img alt="..." src="<?php echo $local ?>/images/users/user_group.webp" />
+                <div class="we__bring-row">
+                    <div class="we__bring-row-left">
+                        <img alt="..." src="<?php echo $local ?>/images/users/user_group.webp" />
                     </div>
                     <div class="we__bring-row-right">
-                        asdasd
+                        <div class="we__bring-row-right-title">
+                            CHÚNG TÔI MANG ĐẾN
+                        </div>
+                        <div class="we__bring-row-right-text">
+                            Đa Khoa Nhật Việt luôn nỗ lực để xây dựng một thương hiệu chăm sóc sức khỏe với hàm lượng tri thức, khoa học và công nghệ ở mức cao. Mỗi dịch vụ y tế đều hướng tới tính tiện ích, hiệu quả, có ý nghĩa với cuộc sống và sức khỏe con người.
+                        </div>
+                        <button class="we__bring-row-right-button">
+                            XEM TẤT CẢ CÁC DỊCH VỤ CỦA CHÚNG TÔI
+                        </button>
                     </div>
                 </div>
             </div>
         </section>
+        <section id="experienced" class="experienced">
+            <div class="experienced__row">
+                <div class="experienced__row-left">
+                    <div class="experienced__row-left-title">
+                        VỀ CHÚNG TÔI
+                    </div>
+                    <div class="experienced__row-left-title1">
+                        ĐỘI NGŨ BÁC SĨ CHUYÊN KHOA
+                    </div>
+                    <div class="experienced__row-left-text">
+                        Y, Bác sĩ của phòng khám đều là những nhân tố giỏi. Họ đã từng công tác tại nhiều bệnh viện lớn trong cả nước. Mỗi chuyên gia đảm nhận một chuyên khoa riêng biệt. Chính điều này sẽ giúp quá trình khám chữa bệnh đạt kết quả tối ưu.
+                    </div>
+                    <div class="experienced__row-left-center">
+                        <button>CHUYÊN NGHIỆP</button>
+                        <button>TẬN TÂM</button>
+                        <button>KINH NGHIỆM</button>
+                    </div>
+                    <div class="experienced__row-left-bottom">
+                        <div class="experienced__row-left-bottom-item">
+                            <div>
+                                <amp-img class="experienced__row-left-bottom-item-img" src="<?php echo $local ?>/images/icons/icon_check.webp" height="33px" width="40px" alt="..."></amp-img>
+                            </div>
+                            <span>Y khoa tiên tiến, dịch vụ tận tâm</span>
+                        </div>
+                        <div class="experienced__row-left-bottom-item">
+                            <div>
+                                <amp-img class="experienced__row-left-bottom-item-img" src="<?php echo $local ?>/images/icons/icon_check.webp" height="33px" width="40px" alt="..."></amp-img>
+                            </div>
+                            <span>Chuyên môn cao, tâm huyết lớn</span>
+                        </div>
+
+                    </div>
+                    <div class="experienced__row-left-bottom">
+                        <div class="experienced__row-left-bottom-item">
+                            <div>
+                                <amp-img class="experienced__row-left-bottom-item-img" src="<?php echo $local ?>/images/icons/icon_check.webp" height="33px" width="40px" alt="..."></amp-img>
+                            </div>
+                            <span>Nơi sức khỏe được ưu tiên</span>
+                        </div>
+                        <div class="experienced__row-left-bottom-item">
+                            <div>
+                                <amp-img class="experienced__row-left-bottom-item-img" src="<?php echo $local ?>/images/icons/icon_check.webp" height="33px" width="40px" alt="..."></amp-img>
+                            </div>
+                            <span>Sức khỏe là hạnh phúc, chúng tôi là người bảo vệ</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="experienced__row-right">
+                    <amp-img class="experienced__row-left-bottom-item-img" src="<?php echo $local ?>/images/users/user-group1.webp" height="490px" width="350px" alt="..."></amp-img>
+                </div>
+            </div>
+            <div class="experienced__bottom">
+                <div class="experienced__list">
+                    <div class="experienced__list-item" >
+                        <img src="<?php echo $local ?>/images/users/user_1.webp" alt="..."></img>
+                        <div>
+                            LÊ BẢN BÌNH
+                        </div>
+                        <div>CK: Ngoại khoa</div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
     </main>
 
 
-<?php include 'inc/footer.php' ?>
+    <?php include 'inc/footer.php' ?>
