5c0e37f7a4ef6944bd29348f914067706d7799a9
trannhon70 <kevintran351996@gmail.com>
2025-03-24 10:04:10 +0700

    update

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
-// define("DB_HOST", "localhost");
-// define("DB_USER", "root");
-// define("DB_PASS", "");
-// define("DB_NAME", "phongkhamdakhoanhatviet.vn");
-
-
 define("DB_HOST", "localhost");
-define("DB_USER", "phongkhamnhatvie");
-define("DB_PASS", "EkGpWz624BASCfTy");
-define("DB_NAME", "phongkhamnhatvie");   
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "phongkhamdakhoanhatviet.vn");
+
+
+// define("DB_HOST", "localhost");
+// define("DB_USER", "phongkhamnhatvie");
+// define("DB_PASS", "EkGpWz624BASCfTy");
+// define("DB_NAME", "phongkhamnhatvie");   
 ?>
